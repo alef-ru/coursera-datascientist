975a58288ed026acf499ef3b1634593823b9754b
Alexander Efremov <a.s.efremov@gmail.com>
2014-04-20 22:43:29 +0400

    Just for a test

--- R-programming-week2/pollutantmean.R
+++ R-programming-week2/pollutantmean.R
@@ -1,14 +1,14 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  acc= c()
+  all.observation= c()
   for (i in id){
     file.name = sprintf('%03i.csv',i)
     df = read.csv(paste(directory,file.name, sep='/'))
-    acc = if (pollutant == 'sulfate')
-      c(acc, df$sulfate)
+    all.observation = if (pollutant == 'sulfate')
+      c(all.observation, df$sulfate)
     else if (pollutant == 'nitrate')
-      c(acc, df$nitrate)
+      c(all.observation, df$nitrate)
     else 
       NA 
   }
-  mean(acc, na.rm=T)
+  mean(all.observation, na.rm=T)
 }
